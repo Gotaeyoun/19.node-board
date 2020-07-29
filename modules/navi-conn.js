c09ad294c90aba917bd03d2b8aef7b88dc4f2da6
Gotaeyoun <ohdin486@gmail.com>
2020-07-29 10:01:00 +0900

    ㄱㄱ

--- modules/navi-conn.js
+++ modules/navi-conn.js
@@ -1,10 +1,12 @@
 module.exports = [
 	{ name: '방명록.MySQL', link: '/gbook' },
 	{ name: '방명록.API', link: '/html/gbook.html' },
-	{ name: '게시판.MySQL', link: '/board' },
-	{ name: '갤러리.Sequelize', link: '/gallery' },
-	{ name: '트위터.MongoDB', link: '/twitter' },
+	{ name: '갤러리.MySQL', link: '/board' },
+	{ name: 'Sequelize', link: '/Sequelize' },
+	{ name: 'MongoDB', link: '/mongoose' },
 	{ name: '회원가입.Passport', link: '/member/join' },
 	{ name: '로그인', link: '/member/login' },
 	{ name: '로그아웃', link: '/member/logout' }
 ];
+
+//분리하고싶으면 다 만들어진 상태에서 29일 아침꺼 ㄱㄱ
