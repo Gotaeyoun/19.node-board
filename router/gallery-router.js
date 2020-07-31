958779824c86bf608e2c5f3d2dbe4d3af9955a96
Gotaeyoun <ohdin486@gmail.com>
2020-07-31 12:01:17 +0900

    연습중

--- router/gallery-router.js
+++ router/gallery-router.js
@@ -1,32 +1,58 @@
 const express = require('express');
 const router = express.Router();
-const pug = {headTitle:"Node/Express 갤러리", css: "gallery", js:"gallery"};
-const {	upload	} = require('../modules/multer-con');
+const pug = {headTitle: "Node/Express 갤러리", css: "gallery", js: "gallery"};
+const { pool } = require('../modules/mysql-conn');
+const { upload } = require('../modules/multer-conn');
 
-router.get(['/','/list','/list/:page'],(req,res,next) =>{
-	res.send('리스트');
+router.get(['/', '/list', '/list/:page'], (req, res, next) => {
+	pug.title = '갤러리 리스트'
+	res.render('gallery/gallery-li.pug', pug);
 });
 
-router.get(['/wr','/wr:/id'],(req,res,next) =>{
-	pug.title = '갤러리 등록'
-	res.render('gallery/gallery-wr.pug',pug);
+router.get(['/wr', '/wr/:id'], (req, res, next) => {
+	pug.title = '갤러리 등록';
+	res.render('gallery/gallery-wr.pug', pug);
 });
 
-
-router.get('/view/:id',(req,res,next) =>{
-	res.send({	title: "상세보기"	});
+router.get('/view/:id', (req, res, next) => {
+	res.json({ title: "상세보기" });
 });
 
-router.get('/rev/:id',(req,res,next) =>{
-	res.send('삭제');
+router.get('/rev/:id', (req, res, next) => {
+	res.send("글삭제");
 });
 
-router.get('/download/:file',(req,res,next) =>{
+router.get('/downlaod/:file', (req, res, next) => {
 	res.send("다운로드");
 });
 
-router.post('/save',upload.single('upfile'),(req,res,next) =>{
-	res.send("저장");
-})
+router.post('/save', upload.array('upfile'), async (req, res, next) => {
+	let sql, sqlVal = [], connect, result;
+	if(req.banExt) {
+		res.send(`<script>alert('${req.banExt} 타입은 업로드 할 수 없습니다.')</script>`);
+	}
+	else {
+		try {
+			sqlVal[0] = req.body.title;
+			sqlVal[1] = req.body.writer;
+			sqlVal[2] = req.body.content;
+			sql = 'INSERT INTO gallery SET title=?, writer=?, content=?';
+			for(let i in req.files) {
+				if(i == 0) sql += ', realfile=?, savefile=?';
+				else sql += ', realfile'+(Number(i)+1)+'=?, savefile'+(Number(i)+1)+'=?';
+				sqlVal.push(req.files[i].originalname);
+				sqlVal.push(req.files[i].filename);
+			}
+			const connect = await pool.getConnection();
+			const result = await connect.execute(sql, sqlVal);
+			connect.release();
+			res.json(result[0]);
+		}
+		catch(e) {
+			console.log(e);
+			next(e);
+		}
+	}
+});
 
 module.exports = router;
