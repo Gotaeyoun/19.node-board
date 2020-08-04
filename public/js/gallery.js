760b384758e35e52e068896e1df74e037ad72d0a
Gotaeyoun <ohdin486@gmail.com>
2020-08-04 10:51:42 +0900

    대화합시다

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,17 +1,60 @@
-var grid = GridStack.init({
-	animate : true,
-	column: 2
+var $grid = $('.list-wrapper').imagesLoaded( function() {
+	$(".loader").hide();
+	$grid.css("visibility", "visible");
+	$grid.masonry({
+		itemSelector: '.list',
+		columnWidth: '.list-sizer',
+		percentPosition: true
+	});
 });
 
-function onGallerySubmit(f){
-	var title = f.title.value.trim()
+function onModal(id) {
+	$.get('/gallery/view/'+id, function(r){
+		var src, src2;
+		if(r.savefile){
+			src = '/upload/' + r.savefile.substr(0, 6) + '/' + r.savefile;
+			$(".modal-wrapper").find(".def-img").attr("src", src);
+		}
+		if(r.savefile){
+			'/upload/' + r.savefile2.substr(0, 6) + '/' + r.savefile2;
+		$(".modal-wrapper").find(".hover-img").attr("src", src2);
+		$(".modal-wrapper").css("display", "flex");
+		}
+	});
+}
+
+$(".modal-wrapper .bt-close").click(function(){
+	$(".modal-wrapper").css("display", "none");
+});
+
+$(".modal-wrapper .bt-def").click(function(){
+	$(".modal-wrapper").find(".def-img").stop().animate({"opacity": 1}, 500);
+	$(".modal-wrapper").find(".hover-img").stop().animate({"opacity": 0}, 500);
+});
+
+$(".modal-wrapper .bt-hover").click(function(){
+	$(".modal-wrapper").find(".def-img").stop().animate({"opacity": 0}, 500);
+	$(".modal-wrapper").find(".hover-img").stop().animate({"opacity": 1}, 500);
+});
+
+
+
+
+/* var grid = GridStack.init({
+	animate: true,
+}); */
+
+
+
+function onGallerySubmit(f) {
+	var title = f.title.value.trim();
 	var writer = f.writer.value.trim();
-	if(title == ''){
+	if(title == '') {
 		alert("제목을 작성해 주세요.");
 		f.title.focus();
 		return false;
 	}
-	if(writer == ''){
+	if(writer == '') {
 		alert("작성자를 기재해 주세요.");
 		f.writer.focus();
 		return false;
--- router/gallery-router.js
+++ router/gallery-router.js
@@ -2,24 +2,37 @@ const express = require('express');
 const router = express.Router();
 const pug = {headTitle: "Node/Express 갤러리", css: "gallery", js: "gallery"};
 const { pool } = require('../modules/mysql-conn');
-const { upload } = require('../modules/multer-con');
+const { upload } = require('../modules/multer-conn');
+const pagerInit = require('../modules/pager-conn');
 
-let sql,sqlVal=[],connect,result;
+let sql, sqlVal = [], connect, result, pager;
 
 router.get(['/', '/list', '/list/:page'], async (req, res, next) => {
-	pug.title = '갤러리 리스트';
-	try{
-		sql = 'SELECT * FROM gallery ORDER BY id DESC';
+	pug.title = '갤러리 리스트';	
+	try {
+		req.query.cnt = req.query.cnt || 15;
+		pager = await pagerInit(req, '/gallery/list', 'gallery');
+		sql = 'SELECT * FROM gallery ORDER BY id DESC LIMIT ?, ?';
+		sqlVal = [pager.stRec, pager.cnt];
 		connect = await pool.getConnection();
-		result = await connect.execute(sql,sqlVal);
+		result = await connect.execute(sql, sqlVal);
 		connect.release();
-		pug.lists = result[0]
-		for(let v of pug.lists){
-			v.src='/upload/' + v.savefile.substr(0,6) + '/' + v.savefile;
+		pug.pager = pager;
+		pug.lists = result[0];
+		for(let v of pug.lists) {
+			v.src = '//via.placeholder.com/300';
+			v.src2 = v.src;
+			if(v.savefile) {
+				v.src = '/upload/' + v.savefile.substr(0, 6) + '/' + v.savefile;
+				v.src2 = v.src;
+			}
+			if(v.savefile2) {
+				v.src2 = '/upload/' + v.savefile2.substr(0, 6) + '/' + v.savefile2;
+			}
 		}
 		res.render('gallery/gallery-li.pug', pug);
 	}
-	catch(e){
+	catch(e) {
 		console.log(e);
 		next(e);
 	}
@@ -30,8 +43,18 @@ router.get(['/wr', '/wr/:id'], (req, res, next) => {
 	res.render('gallery/gallery-wr.pug', pug);
 });
 
-router.get('/view/:id', (req, res, next) => {
-	res.json({ title: "상세보기" });
+router.get('/view/:id', async (req, res, next) => {
+	let id = req.params.id;
+	try {
+		sql = 'SELECT savefile, savefile2 FROM gallery WHERE id=' + id;
+		connect = await pool.getConnection();
+		result = await connect.execute(sql);
+		connect.release();
+		res.json(result[0][0]);
+	}
+	catch(e) {
+		next(e);
+	}
 });
 
 router.get('/rev/:id', (req, res, next) => {
@@ -61,7 +84,7 @@ router.post('/save', upload.array('upfile'), async (req, res, next) => {
 			const connect = await pool.getConnection();
 			const result = await connect.execute(sql, sqlVal);
 			connect.release();
-			res.redirect('/gallery/');
+			res.redirect('/gallery');
 		}
 		catch(e) {
 			console.log(e);
