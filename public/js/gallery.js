c7676805c4a4ee26ed1bc1ff21b716ff4dbf8af7
Gotaeyoun <ohdin486@gmail.com>
2020-08-06 13:07:49 +0900

    연습중

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -8,18 +8,39 @@ var $grid = $('.list-wrapper').imagesLoaded( function() {
 	});
 });
 
-function onModal(id) {
+var id, seq;
+
+function onDetail() {
+	$(".modal-wing").css({"opacity": 1, "transform": "translateX(0)"});
+}
+
+$(".bt-hide").click(function(){
+	$(".modal-wing").css({"opacity": 0, "transform": "translateX(-500px)"});
+});
+
+function onDownload() {
+	// /gallery/download/3?seq=2, /gallery/download/3?seq=
+	location.href = '/gallery/download/'+id+'?seq='+seq;
+}
+
+function onModal(idx) {
+	id = idx;
 	$.get('/gallery/view/'+id, function(r){
 		var src, src2;
-		if(r.savefile){
+		if(r.savefile) {
 			src = '/upload/' + r.savefile.substr(0, 6) + '/' + r.savefile;
 			$(".modal-wrapper").find(".def-img").attr("src", src);
+			$(".modal-wrapper").find(".bt-def").show();
 		}
-		if(r.savefile){
-			'/upload/' + r.savefile2.substr(0, 6) + '/' + r.savefile2;
-		$(".modal-wrapper").find(".hover-img").attr("src", src2);
+		if(r.savefile2) {
+			src2 = '/upload/' + r.savefile2.substr(0, 6) + '/' + r.savefile2;
+			$(".modal-wrapper").find(".hover-img").attr("src", src2);
+			$(".modal-wrapper").find(".bt-hover").show();
+		}
+		$(".modal-wing").find(".title").html(r.title);
+		$(".modal-wing").find(".writer").html(r.writer);
+		$(".modal-wing").find(".content").html(r.content);
 		$(".modal-wrapper").css("display", "flex");
-		}
 	});
 }
 
@@ -30,15 +51,51 @@ $(".modal-wrapper .bt-close").click(function(){
 $(".modal-wrapper .bt-def").click(function(){
 	$(".modal-wrapper").find(".def-img").stop().animate({"opacity": 1}, 500);
 	$(".modal-wrapper").find(".hover-img").stop().animate({"opacity": 0}, 500);
-});
+	$(".modal-wrapper").find(".modal-pager > i").removeClass("active");
+	$(this).addClass("active");
+	seq = "";
+}).trigger("click");
 
 $(".modal-wrapper .bt-hover").click(function(){
 	$(".modal-wrapper").find(".def-img").stop().animate({"opacity": 0}, 500);
 	$(".modal-wrapper").find(".hover-img").stop().animate({"opacity": 1}, 500);
+	$(".modal-wrapper").find(".modal-pager > i").removeClass("active");
+	$(this).addClass("active");
+	seq = "2";
 });
 
+function onChg(id) {
+	location.href = '/gallery/wr/'+id;
+}
 
+function onRev(id, savefile, savefile2) {
+	if(confirm("정말로 삭제하시겠습니까?")) {
+		location.href = '/gallery/rev/'+id+'?savefile='+savefile+'&savefile2='+savefile2;
+	}
+}
 
+function onImgRev(id, n, file) {
+	if(confirm("이미지를 삭제하시겠습니까?")) {
+		$.get('/gallery/api-img/'+id, {n: n, file: file}, function(r) {
+			if(r.code == 200) {
+				if(n == '') $(".wr-wrap .img1").remove();
+				else $(".wr-wrap .img2").remove();
+			}
+			else {
+				console.log(r.error);
+			}
+		});
+	}
+}
+
+/*
+$(".wr-wrap .fa-question").mouseenter(function(){
+	$(this).find("span").css("opacity", 1);
+});
+$(".wr-wrap .fa-question").mouseleave(function(){
+	$(this).find("span").css("opacity", 0);
+});
+*/
 
 /* var grid = GridStack.init({
 	animate: true,
--- router/gallery-router.js
+++ router/gallery-router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const fs = require('fs');
 const pug = {headTitle: "Node/Express 갤러리", css: "gallery", js: "gallery"};
-const { pool } = require('../modules/mysql-conn');
+const { pool, mysqlErr, queryExecute, fileRev, uploadPath, storagePath } = require('../modules/mysql-conn');
 const { upload } = require('../modules/multer-conn');
 const pagerInit = require('../modules/pager-conn');
 
-let sql, sqlVal = [], connect, result, pager;
+let sql, sqlVal = [], result, pager;
 
 router.get(['/', '/list', '/list/:page'], async (req, res, next) => {
 	pug.title = '갤러리 리스트';	
@@ -14,82 +16,135 @@ router.get(['/', '/list', '/list/:page'], async (req, res, next) => {
 		pager = await pagerInit(req, '/gallery/list', 'gallery');
 		sql = 'SELECT * FROM gallery ORDER BY id DESC LIMIT ?, ?';
 		sqlVal = [pager.stRec, pager.cnt];
-		connect = await pool.getConnection();
-		result = await connect.execute(sql, sqlVal);
-		connect.release();
+		result = await queryExecute(sql, sqlVal);
 		pug.pager = pager;
-		pug.lists = result[0];
+		pug.lists = result;
 		for(let v of pug.lists) {
 			v.src = '//via.placeholder.com/300';
 			v.src2 = v.src;
-			if(v.savefile) {
-				v.src = '/upload/' + v.savefile.substr(0, 6) + '/' + v.savefile;
+			if(v.savefile || v.savefile == '') {
+				v.src = uploadPath(v.savefile);
 				v.src2 = v.src;
 			}
-			if(v.savefile2) {
-				v.src2 = '/upload/' + v.savefile2.substr(0, 6) + '/' + v.savefile2;
+			if(v.savefile2 || v.savefile2 == '') {
+				v.src2 = uploadPath(v.savefile2);
 			}
 		}
 		res.render('gallery/gallery-li.pug', pug);
 	}
 	catch(e) {
-		console.log(e);
 		next(e);
 	}
 });
 
-router.get(['/wr', '/wr/:id'], (req, res, next) => {
-	pug.title = '갤러리 등록';
+router.get(['/wr', '/wr/:id'], async (req, res, next) => {
+	let id = req.params.id;
+	if(!id) {
+		pug.title = '갤러리 등록';
+		pug.list = null;
+	}
+	else {
+		pug.title = '갤러리 수정';
+		sql = 'SELECT * FROM gallery WHERE id='+id;
+		result = await queryExecute(sql);
+		pug.list = result[0];
+		if(pug.list.savefile) pug.list.src = uploadPath(pug.list.savefile);
+		if(pug.list.savefile2) pug.list.src2 = uploadPath(pug.list.savefile2);
+	}
 	res.render('gallery/gallery-wr.pug', pug);
 });
 
 router.get('/view/:id', async (req, res, next) => {
 	let id = req.params.id;
 	try {
-		sql = 'SELECT savefile, savefile2 FROM gallery WHERE id=' + id;
-		connect = await pool.getConnection();
-		result = await connect.execute(sql);
-		connect.release();
-		res.json(result[0][0]);
+		sql = 'SELECT * FROM gallery WHERE id=' + id;
+		result = await queryExecute(sql);
+		res.json(result[0]);
+	}
+	catch(e) {
+		res.json(e);
+	}
+});
+
+router.get('/rev/:id', async (req, res, next) => {
+	try {
+		let id = req.params.id;
+		let savefile = req.query.savefile;
+		let savefile2 = req.query.savefile2;
+		if(savefile) await fileRev(savefile);
+		if(savefile2) await fileRev(savefile2);
+		sql = 'DELETE FROM gallery WHERE id='+id;
+		result = await queryExecute(sql);
+		res.redirect('/gallery');
 	}
 	catch(e) {
 		next(e);
 	}
 });
 
-router.get('/rev/:id', (req, res, next) => {
-	res.send("글삭제");
+router.get('/download/:id', async (req, res, next) => {
+	let id = req.params.id;
+	let seq = req.query.seq;
+	let savefile, realfile;
+	try {
+		sql = `SELECT savefile${seq}, realfile${seq} FROM gallery WHERE id=${id}`;
+		result = await queryExecute(sql);
+		savefile = result[0][`savefile${seq}`]; // result[0][0]['savefile2'] == result[0][0].savefile2
+		realfile = result[0][`realfile${seq}`];
+		savefile = storagePath(savefile);
+		// C:\Users\hi\Documents\임덕규\20.node-board\storage\200731\200731-sdfj-sdjf...jpg
+		res.download(savefile, realfile);
+	}
+	catch(e) {
+		next(e);
+	}
 });
 
-router.get('/downlaod/:file', (req, res, next) => {
-	res.send("다운로드");
-});
-
-router.post('/save', upload.array('upfile'), async (req, res, next) => {
+router.post('/save', upload.fields([{name: 'upfile'}, {name: 'upfile2'}]), async (req, res, next) => {
+	let { id, savefile, savefile2, title, writer, content } = req.body;
 	if(req.banExt) {
 		res.send(`<script>alert('${req.banExt} 타입은 업로드 할 수 없습니다.')</script>`);
 	}
 	else {
 		try {
-			sqlVal[0] = req.body.title;
-			sqlVal[1] = req.body.writer;
-			sqlVal[2] = req.body.content;
-			sql = 'INSERT INTO gallery SET title=?, writer=?, content=?';
-			for(let i in req.files) {
-				if(i == 0) sql += ', realfile=?, savefile=?';
-				else sql += ', realfile'+(Number(i)+1)+'=?, savefile'+(Number(i)+1)+'=?';
-				sqlVal.push(req.files[i].originalname);
-				sqlVal.push(req.files[i].filename);
+			sqlVal[0] = title;
+			sqlVal[1] = writer;
+			sqlVal[2] = content;
+			if(id) sql = 'UPDATE gallery SET title=?, writer=?, content=?';
+			else sql = 'INSERT INTO gallery SET title=?, writer=?, content=?';
+			if(req.files['upfile']) {
+				if(id && savefile) await fileRev(savefile);
+				sql += ', realfile=?, savefile=?';
+				sqlVal.push(req.files['upfile'][0].originalname);
+				sqlVal.push(req.files['upfile'][0].filename);
 			}
-			const connect = await pool.getConnection();
-			const result = await connect.execute(sql, sqlVal);
-			connect.release();
+			if(req.files['upfile2']) {
+				if(id && savefile2) await fileRev(savefile2);
+				sql += ', realfile2=?, savefile2=?';
+				sqlVal.push(req.files['upfile2'][0].originalname);
+				sqlVal.push(req.files['upfile2'][0].filename);
+			}
+			if(id) sql += ' WHERE id='+id;
+			await queryExecute(sql, sqlVal);
 			res.redirect('/gallery');
 		}
 		catch(e) {
-			console.log(e);
 			next(e);
 		}
+	}
+});
+
+router.get('/api-img/:id', async (req, res, next) => {
+	try {
+		let id = req.params.id;
+		let { n, file } = req.query;
+		sql = `UPDATE gallery SET savefile${n}=NULL, realfile${n}=NULL WHERE id=${id}`;
+		await queryExecute(sql);
+		result = await fileRev(file);
+		res.json(result);
+	}
+	catch(e) {
+		res.json(e);
 	}
 });
 
