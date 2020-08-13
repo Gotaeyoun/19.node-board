4169544889d9ee77cb7c6ddbd816661fed0bd15b
Gotaeyoun <ohdin486@gmail.com>
2020-08-13 11:52:16 +0900

    수정완료

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const passport = require('passport');
 require('dotenv').config();
 const session = require('./modules/session-conn');
 const logger = require('./modules/morgan-conn');
-const passportModule = require('./passport/index');
+const passportModule = require('./passport');
 
 
 /*************** 내부모듈 *****************/
@@ -39,21 +39,22 @@ app.set('views', viewsPath);
 app.locals.pretty = true;
 app.locals.headTitle = '노드 게시판';
 app.locals.navis = navi;
-app.use((req, res, next) => {
-	app.locals.user = req.session.user ? req.session.user : {};
-	next();
-});
+
 
 /***** AJAX/POST 데이터를 json으로 변경 ******/
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
 /***** logger(morgan) Init ******/
 passportModule(passport);
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use((req, res, next) => {
+	// app.locals.user = req.session.user ? req.session.user : {}; //session
+	console.log(req.user);
+	app.locals.user = req.user ? req.user : {};
+	next();
+});
 
 /***** logger(morgan) Init ******/
 app.use(logger);
--- passport/kakao.js
+++ passport/kakao.js
@@ -1,8 +1,8 @@
-const localStrategy = require('passport-local').Strategy;
-const bcrypt = require('bycrypt');
-const { } = require('../modules/mysql-conn') = require('../modules/mysql-conn');
+const KakaoStrategy = require('passport-kakao').Strategy;
+const bcrypt = require('bcrypt');
+const { queryExecute, mysqlErr } = require('../modules/mysql-conn');
 
 
-module.exports = (passport) =>{
+module.exports = (passport) => {
 	
 };
